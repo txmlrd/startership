464847d0d51043aa6e1e4c055cd827da62659b1a
txmlrd <gungadhisanjaya@gmail.com>
2025-02-07 21:35:33 +0800

    fixed luci code on navbar using link instead of a tag

--- components/basic/Navbar.tsx
+++ components/basic/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "../reusable/Button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="flex flex-row items-center justify-between bg-white text-black p-4 font-bold">
-      <a href="/">Home</a>
+      <Link href="/">Home</Link>
       <Button link="/service">Service</Button>
       <a href="/contact">Contact</a>
     </div>
